a646b26bb9a61662fdf18afe811cbe8e7117426a
busy-gnl <vallantjesse@live.com>
2022-12-02 10:18:57 +0100

    completed quest15

--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -6,15 +6,17 @@ use App\Entity\Season;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class SeasonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('number')
-            ->add('year')
-            ->add('description')
+            ->add('number', IntegerType::class)
+            ->add('year', IntegerType::class)
+            ->add('description', TextType::class)
             ->add('program', null, ['choice_label' => 'title']);
     }
 
