5bf015f99d4fc05704a09b3cf8a2c6e0319b5063
busy-gnl <vallantjesse@live.com>
2022-12-02 10:08:51 +0100

    completed quest14

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -6,17 +6,20 @@ use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ProgramType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('synopsis')
-            ->add('poster')
-            ->add('country')
-            ->add('year')
+            ->add('title', TextType::class)
+            ->add('synopsis', TextType::class)
+            ->add('poster', TextType::class)
+            ->add('country', CountryType::class)
+            ->add('year', IntegerType::class)
             ->add('category', null, ['choice_label' => 'name']);
     }
 
